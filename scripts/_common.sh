918b0ca6c4bce1f67b38821042952216d99ddb91
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-01-27 08:59:29 +0100

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,6 +3,7 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+
 go_version="1.22"
 
 app_path="$install_dir/go/src/github.com/georgyo/ifconfig.io"
